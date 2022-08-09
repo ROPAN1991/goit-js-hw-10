f700cf5588752b97991501705e0318d92a8f5df1
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-08-09 13:27:47 +0300

    hwgoit10final

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,7 @@
-export const fetchCountries = (name) => {
-  return fetch('https://restcountries.com/v3.1/name/' + name + '/?fields=name,capital,population,flags,languages')
-}
+export const fetchCountries = name => {
+  return fetch(
+    'https://restcountries.com/v3.1/name/' +
+      name +
+      '/?fields=name,capital,population,flags,languages'
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries'
-import debounce from 'lodash.debounce'
-import Notiflix from 'notiflix'
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-const countryList = document.querySelector('.country-list')
-const countryInfo = document.querySelector('.country-info')
-const input = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const input = document.querySelector('#search-box');
 
 function renderCountryFromList(country) {
-  const {name: {official}, flags} = country
+  const {
+    name: { official },
+    flags,
+  } = country;
   return `
     <li>
       <img src='${flags.svg}' alt='flag'>
       <span>${official}</span>
     </li>
-  `
+  `;
 }
 
 function renderCountry(country) {
-  const {name: {official}, capital: [capital], population, flags, languages} = country
-  const lngs = Object.values(languages).join(', ')
+  const {
+    name: { official },
+    capital: [capital],
+    population,
+    flags,
+    languages,
+  } = country;
+  const lngs = Object.values(languages).join(', ');
   return `
     <div><img src='${flags.svg}' alt='flag'> <h1>${official}</h1></div>
     <ul>
@@ -29,47 +38,47 @@ function renderCountry(country) {
       <li><b>Population:</b> <span>${population}</span></li>
       <li><b>Languages:</b> <span>${lngs}</span></li>
     </ul>
-  `
+  `;
 }
 
-const onInput = async (e) => {
-  const value = e.target.value.trim()
+const onInput = async e => {
+  const value = e.target.value.trim();
 
-  if (!value.length) return
+  if (!value.length) return;
 
   try {
-    const response = await fetchCountries(value)
-    const countries = await response.json()
+    const response = await fetchCountries(value);
+    const countries = await response.json();
 
-    countryList.innerHTML = ''
-    countryInfo.innerHTML = ''
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
 
     if (countries.length > 10) {
-      Notiflix.Notify.warning("Too many matches found. Please enter a more specific name.");
+      Notiflix.Notify.warning(
+        'Too many matches found. Please enter a more specific name.'
+      );
     } else if (countries.length > 2 && countries.length < 10) {
-      let html = ''
+      let html = '';
       countries.forEach(country => {
-        html += renderCountryFromList(country)
-      })
-      countryList.insertAdjacentHTML('beforeend', html)
-
+        html += renderCountryFromList(country);
+      });
+      countryList.insertAdjacentHTML('beforeend', html);
     } else if (countries.length === 1) {
-      let html = ''
+      let html = '';
       countries.forEach(country => {
-        html += renderCountry(country)
-      })
-      countryInfo.insertAdjacentHTML('beforeend', html)
-
+        html += renderCountry(country);
+      });
+      countryInfo.insertAdjacentHTML('beforeend', html);
     } else {
       if (response.status === 404) {
-        Notiflix.Notify.failure("Oops, there is no country with that name");
+        Notiflix.Notify.failure('Oops, there is no country with that name');
       }
     }
   } catch {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
+    Notiflix.Notify.failure('Oops, there is no country with that name');
   }
-}
+};
 
-const debouncedOnInput = debounce(onInput, DEBOUNCE_DELAY)
+const debouncedOnInput = debounce(onInput, DEBOUNCE_DELAY);
 
-input.addEventListener('input', debouncedOnInput)
+input.addEventListener('input', debouncedOnInput);
